360d85d1314ddbc23ef66676dfa6923cd483eb9c
Raman A. Shah <raman.anand.shah@gmail.com>
2017-05-14 17:32:59 -0500

    Build POC parsing a data element using yaml-light.
    stack.yaml, bread.cabal: Add dependencies.
    Main.hs: Pull out an amount (as string) from a YAML RECORD.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,21 @@
-module Main where
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Yaml.YamlLight as Y
+
+fromJust :: Maybe a -> a
+fromJust (Just x) = x
+fromJust Nothing = error "Nope!"
+
+getFromArray :: Int -> Y.YamlLight -> Y.YamlLight
+getFromArray i = (!! i) . fromJust . Y.unSeq
+
+getFromMap :: String -> Y.YamlLight -> Y.YamlLight
+getFromMap key = fromJust . Y.lookupYL (Y.YStr $ BS.pack key)
+
+getString :: Y.YamlLight -> String
+getString = BS.unpack . fromJust . Y.unStr
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+   x <- Y.parseYamlFile "./doc/examples/ciabatta.yml"
+   putStrLn $ getString $ getFromMap "amount" $ getFromArray 2 $
+     getFromMap "ingredients" $ getFromArray 1 $ x
