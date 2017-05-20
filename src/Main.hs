9875faa72d9be257fb5f7267ad0e804ee1c1b8f4
Raman A. Shah <raman.anand.shah@gmail.com>
2017-05-20 15:21:31 -0500

    Configure source YAML and scale factor from the command line.

--- src/Main.hs
+++ src/Main.hs
@@ -7,14 +7,20 @@ import qualified Data.Yaml as Y
 import Data.Yaml (FromJSON(..), (.:))
 import qualified Data.ByteString.Char8 as BS
 import Control.Applicative
+import System.Environment
 import BreadData
 import BreadUtils
 
 main :: IO ()
 main = do
-  x <- readFile("./doc/examples/ciabatta.yml")
-  parsed <- return ((Y.decodeEither $ BS.pack x) :: Either String [Section])
+  -- Read in command-line arguments: source YAML and scale factor
+  args <- getArgs
+  recipeYaml <- readFile(args !! 0)
+  scaleFactor <- return (read (args !! 1) :: Float)
+
+  parsed <- return ((Y.decodeEither $
+    BS.pack recipeYaml) :: Either String [Section])
   case parsed of
     Left s -> putStrLn $ "Parse error: " ++ s
     Right recipe -> do
-      print $ scaleRecipe 1.2 recipe
+      print $ scaleRecipe scaleFactor recipe
