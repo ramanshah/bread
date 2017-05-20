885922ac8bf0582694a66c0d578b6c8ef50fb531
Raman A. Shah <raman.anand.shah@gmail.com>
2017-05-20 16:09:58 -0500

    Main.hs: Clean up a bit.

--- src/Main.hs
+++ src/Main.hs
@@ -2,25 +2,31 @@
 
 module Main where
 
-import Data.Text (Text)
 import qualified Data.Yaml as Y
-import Data.Yaml (FromJSON(..), (.:))
 import qualified Data.ByteString.Char8 as BS
 import Control.Applicative
 import System.Environment
+import System.Exit
+
 import BreadData
 import BreadUtils
+
+-- Provide unified way to deal with bad input via the Either monad
+printIfError :: Either String a -> IO a
+printIfError (Left s) = do
+  putStrLn s
+  exitWith (ExitFailure 1)
+printIfError (Right x) = return x
 
 main :: IO ()
 main = do
   -- Read in command-line arguments: source YAML and scale factor
   args <- getArgs
-  recipeYaml <- readFile(args !! 0)
+  recipeYaml <- readFile $ args !! 0
   scaleFactor <- return (read (args !! 1) :: Float)
 
-  parsed <- return ((Y.decodeEither $
+  recipe <- printIfError ((Y.decodeEither $
     BS.pack recipeYaml) :: Either String [Section])
-  case parsed of
-    Left s -> putStrLn $ "Parse error: " ++ s
-    Right recipe -> do
-      print $ scaleRecipe scaleFactor recipe
+
+  print $ scaleRecipe scaleFactor recipe
+  exitWith ExitSuccess
